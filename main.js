faad04160422d0e87625bf766b59795aa714ae22
Andrew Masih <armasih@outlook.com>
2021-01-07 19:17:43 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let age = 25;
+
+(age > 18) ?  "You are soooo mature": "You are still a child";
+
+// The code below is the same logic but written longhand
+if (age > 18) {
+  "You are soooo mature"
+} else {
+  "You are still a child"
+}
